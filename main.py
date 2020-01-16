37b0f4243f5e01028305fdb32ffeb30d1feb948e
xxl <>
2020-01-16 16:09:04 +0800

    写了个爬取马蜂窝产品信息的爬虫

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
+# main.py
 import requests
+import re
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
-def get_one_page(url):
+def get_one_page(page_no):
+    url = "http://www.mafengwo.cn/sales/ajax_2017.php?act=GetContentList&s_dept_time%5B%5D=all&price%5B%5D=all" \
+          "&tag_group%5B9779%5D%5B%5D=all&tag_group%5B9365%5D%5B%5D=all&from=10208&kw=&to=M16209&salesType=NaN&page=" \
+          + str(page_no) + "&group=1&sort=smart&sort_type=desc&limit=100 "
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/79.0.3945.117 Safari/537.36 "
     }
-    response = requests.get(url,headers=headers)
-    if response.status_code==200:
-        return response.text
-    else:
-        return None
+    response = requests.get(url, headers=headers)
+    html = response.json()['html']
+    soup = BeautifulSoup(html, 'lxml')
+    return soup.find_all(name='a', class_='item clearfix')
 
 
 def main():
-    url = "https://maoyan.com/board/4"
-    browser = webdriver.Chrome()
-    browser.get(url)
-    wait = WebDriverWait(browser, 10)   # 指定页面等待时间
-    print(browser.get_cookies())
+    base_url = "www.mafengwo.cn"
+    for i in range(1, 100):  # 1到100页内获取页面,直到没有了为止
+        product_list = get_one_page(i)
 
+        if len(product_list) != 0:
+            for product in product_list:
+                href = base_url + product['href']
+                title = product.find("h3").string.strip(" \n")
+                store = re.findall("店铺:(.*)", product.find(class_='t').string)[0]
+                try:
+                    sold = re.findall("已售(\d*)", product.find(name='div', class_='detail').p.string)[0]
+                except:
+                    sold = 0
+                print(href, title, store, sold)
+        else:
+            break
 
-
-
-    browser.close()
 
 main()
